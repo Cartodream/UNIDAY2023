852aa65d14600f8dfc5f17bc70d568ee51c0711d
Cartodream <90085887+Cartodream@users.noreply.github.com>
2023-05-24 12:41:08 +0200

    Add files via upload

---
+++ donnees/POI.js
@@ -0,0 +1,33 @@
+var poi = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+
+
+{ "type": "Feature", "properties": { "id": "1p04j714drbnw19lr1p04jdv9i0a3n5j", "nom": "Institut Jeanne de France","nombre_enfants":"90", "descriptif": "Goujon Sandra", "tel": "+33686496816", "mail": "goujon.sandra@hotmail.fr", "site_web": "", "sous_cat": "Etablissement Scolaire", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "06000", "commune": "Nice", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [7.23826889465,43.7119992661] } },
+
+{ "type": "Feature", "properties": { "id": "idbjepcrg0yn7bdlwcuwmgzidbjepv5e", "nom": "Ecole maternelle ","nombre_enfants":"60", "descriptif": "DIARRA Sanoussi", "tel": "+33632304370", "mail": "sanoussi.diarra@paris.fr", "site_web": "", "sous_cat": "Etablissement Périscolaire", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "75013", "commune": "PARIS", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [2.33629344655,48.8626304852] } },
+
+{ "type": "Feature", "properties": { "id": "wzob4iw1mq8im91qsiwzobn3viq8y6th", "nom": "Ecole élémentaire La Villedieu ","nombre_enfants":"20", "descriptif": "FERNANDES Doriane", "tel": "+33130512491", "mail": "0780876d@ac-versailles.fr", "site_web": "", "sous_cat": "Etablissement Scolaire", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "78990", "commune": "ELANCOURT", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [1.96049898519,48.7781596518] } },
+
+{ "type": "Feature", "properties": { "id": "hxlxhn7xmwlsj8op1nuhxlxhnnggax45", "nom": "MMA Verduron Bas ","nombre_enfants":"18", "descriptif": "Alcina Laurence", "tel": "+33491517134", "mail": "lalcina@marseille.fr", "site_web": "", "sous_cat": "Etablissement Périscolaire", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "13015", "commune": "marseille", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [5.363407897,43.358794754] } },
+
+{ "type": "Feature", "properties": { "id": "i9n0qee1nehii9n0qwrc6m3lxse8wifz", "nom": "ACCUEIL PERISCOLAIRE DE CLARY ","nombre_enfants":"20", "descriptif": "Autréaux Peggy", "tel": "+33645315536", "mail": "autreaux.peggy@gmail.com", "site_web": "", "sous_cat": "Etablissement Périscolaire", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "59225", "commune": "CLARY", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [3.39589708434,50.0684086956] } },
+
+{ "type": "Feature", "properties": { "id": "xtds4osbptt990kjuxtdsh2zem67obdg", "nom": "ALSH des Iles ","nombre_enfants":"1240", "descriptif": "HUGHES Lucy", "tel": "+33681325720", "mail": "lucy.hughes@agglo-agen.fr", "site_web": "", "sous_cat": "Collectivité", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "47000", "commune": "AGEN", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [0.625583928763,44.2028139104] } },
+
+{ "type": "Feature", "properties": { "id": "vi44jhytatr16zkrty16dvi44jhyjw7n", "nom": "CENTRE SOCIAL LO SOLAN ","nombre_enfants":"80", "descriptif": "vannier thomas", "tel": "+33648055257", "mail": "alsh@lo-solan.org", "site_web": "", "sous_cat": "Etablissement Périscolaire", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "64150", "commune": "MOURENX", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [-0.617464224458,43.3740220656] } },
+
+{ "type": "Feature", "properties": { "id": "ji0lpsh6ru2hqeegyhv8am9ji0lpsh61", "nom": "maternelle Gambetta ","nombre_enfants":"100", "descriptif": "casal marijo", "tel": "+33467878448", "mail": "ce.0340419w@ac-montpellier.fr", "site_web": "", "sous_cat": "Etablissement Scolaire", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "34400", "commune": "lunel", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [4.13566472301,43.6820481757] } },
+
+{ "type": "Feature", "properties": { "id": "devsnmltvr3r9k4opdevs931pxbcb7j4", "nom": "Association Archipel ","nombre_enfants":"200", "descriptif": "MORIS Marie", "tel": "+33965219513", "mail": "laduranne2@asso-archipel.com", "site_web": "", "sous_cat": "Etablissement Périscolaire", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "13290", "commune": "Aix en Provence", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [5.39857444582,43.5360708378] } },
+
+{ "type": "Feature", "properties": { "id": "yl5qcongqaub70wv12jys7yeyl5qcof2", "nom": "kid'o Saulce ","nombre_enfants":"92", "descriptif": "Abdel Yahyaouy Abdel", "tel": "+33626153114", "mail": "abdel.yahyaouy@montelimar-agglo.fr", "site_web": "", "sous_cat": "Etablissement Périscolaire", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "26270", "commune": "Saulce sur Rhone", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [4.79084711633,44.7042406134] } },
+
+{ "type": "Feature", "properties": { "id": "wd59tyehxruepcphwd5ihvm6kb9o3cpu", "nom": "ASSOCIATION COMMUNITY ","nombre_enfants":"120", "descriptif": "PIESSET ALINE", "tel": "+33788295632", "mail": "csjf.community@gmail.com", "site_web": "", "sous_cat": "Etablissement Périscolaire", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "62510", "commune": "ARQUES", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [2.31902794783,50.7402216255] } },
+
+{ "type": "Feature", "properties": { "id": "fy5j4ewhqomyoy54h9higvfy5j4g7kci", "nom": "Ecole Primaire Publique de Saint Ferréol ","nombre_enfants":"50", "descriptif": "DUVAUCHELLE François", "tel": "+33782446620", "mail": "ce.0741318f@ac-grenoble.fr", "site_web": "", "sous_cat": "Etablissement Scolaire", "categorie": "Type Etablissement", "adresse": null, "adresse2": "", "cp": "74210", "commune": "Saint Ferréol", "photo": null, "source": "" }, "geometry": { "type": "Point", "coordinates": [6.30718276137,45.7833203062] } },
+
+]
+};
